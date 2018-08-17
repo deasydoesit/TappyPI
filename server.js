1058bc646b9747b04a9777158f4e2ef2def45650
deasydoesit <darwin.rinderer@gmail.com>
2018-08-16 22:36:36 -0400

    Ts. 2

--- server.js
+++ server.js
@@ -51,7 +51,7 @@ bleno.on('advertisingStart', function(error) {
                         // Create write functionality e.g., where endpoints can send data to Pi
                         onWriteRequest : function(data, offset, withoutResponse, callback) {
                             this.value = data;
-                            console.log(typeof this.value);
+                            console.log(typeof data);
                             // var serializedTx = this.value.toString(16);
                             // console.log(data);
                             // console.log('Write request: value = ' + this.value.toString(16));
